ad01289a7f6e8aaf9bc8e8157cce6010e8311c04
Jonathan Sloan <jsloan117@gmail.com>
2018-08-23 13:45:14 -0500

    Update spec_helper.rb
    testing out codecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
+require 'simplecov'
+SimpleCov.start
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
+
 require 'chefspec'
 require 'chefspec/berkshelf'
