0e70248fbd90b5fce63b5e9a8da811f0268693bd
Jonathan Sloan <jsloan117@gmail.com>
2018-08-25 14:33:23 -0500

    undoing codecov and reporting on travis

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'berkshelf', '~> 7.0'
-gem 'codecov'
 gem 'cookstyle', '~> 3.0'
 gem 'foodcritic', '~> 14.0'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
-require 'simplecov'
-SimpleCov.start
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
-
 require 'chefspec'
 require 'chefspec/berkshelf'
